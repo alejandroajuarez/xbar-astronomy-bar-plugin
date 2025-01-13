14aadfbd180f28b349e59910c548185876c299ae
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-12 17:08:02 -0700

    Added Astronomy App/Secret ID

--- astronomy-bar.1hr.rb
+++ astronomy-bar.1hr.rb
@@ -4,3 +4,24 @@
 # <xbar.author>Alejandro Juarez</xbar.author>
 # <xbar.desc>Shows sun position in menu bar</xbar.desc>
 # <xbar.dependencies>http</xbar.dependencies>
+# <xbar.var>string(ASTRONOMY_APP_ID=""): Astronomy App ID</xbar.var>
+# <xbar.var>string(ASTRONOMY_SECRET=""): Astronomy Secret ID</xbar.var>
+require 'http'
+require 'base64'
+require 'json'
+require 'date'
+
+class AstronomyTracker
+  BASE_URL = 'https://api.astronomyapi.com/api/v2'
+  
+  def initialize(app_id:, app_secret:)
+    @credentials = Base64.strict_encode64("#{app_id}:#{app_secret}")
+  end
+
+
+  
+  # Use environment variables
+tracker = AstronomyTracker.new(
+  app_id: ENV['ASTRONOMY_APP_ID'], 
+  app_secret: ENV['ASTRONOMY_SECRET']
+)
