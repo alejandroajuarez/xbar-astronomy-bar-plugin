a728768d05657542103266fe10c71ed114184f08
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-12 17:04:22 -0700

    Added metadata for xbar

---
+++ astronomy-bar.1hr.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# <xbar.title>Astronomy Tracker</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>Alejandro Juarez</xbar.author>
+# <xbar.desc>Shows sun position in menu bar</xbar.desc>
+# <xbar.dependencies>http</xbar.dependencies>
